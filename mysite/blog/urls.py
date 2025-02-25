dda74b603285ff56a93f5a1e5ffdedcff78a84f5
shaheem <shaheemmahmood728@gmail.com>
2025-02-25 22:49:37 +0530

    Added new views and URLs for post comments and publishing

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -9,5 +9,9 @@ urlpatterns=[
     path('post/new/', views.CreatePostView.as_view(), name='post_new'),
     path('post/<int:pk>/edit/',views.PostUpdateView.as_view,name='post_edit'),
     path('post/<int:pk>/remove/',views.PostDeleteView.as_view,name='post_remove'),
-    path('draft/',views.DraftListView.as_view,name='post_draft_list')
+    path('draft/',views.DraftListView.as_view,name='post_draft_list'),
+    path('post/<int:pk>/comments/',views.add_comment_to_post,name='add_comment_to_post'),
+    path('post/<int:pk>/approve/',views.comment_approve,name='comment_approve'),
+    path('post/<int:pk>/remove/',views.comment_remove,name='comment_remove'),
+    path('post/<int:pk>/publish/',views.post_publish,name='post_publish'),
 ]
